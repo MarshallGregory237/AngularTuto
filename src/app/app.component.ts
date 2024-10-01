c0010a53177f419af3d79b5280420a934be317f3
gregory <gregorysopy@gmail.com>
2024-10-01 10:29:38 +0100

    u

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './components/user-component/user-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    UserComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'my-first-angular-app';
+
+  isLogged = true;
+
+  isServerRunning = true;
+
+  operatingSystems = [
+    {id: 'win', name:'windows'},
+    {id: 'osf', name:'linux'}
+  ]
+
+  users = [
+    {id: 0, name: 'Sarah'},
+    {id: 1, name: 'Amy'},
+    {id: 2, name: 'Rachel'},
+    {id: 3, name: 'Jessica'},
+    {id: 4, name: 'Poornima'}
+  ]
+
 }
